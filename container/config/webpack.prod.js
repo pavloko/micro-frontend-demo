8ea1375e359fca4656b2879813742f3e2d9f431c
pavloko <pavlo.kochubei@gmail.com>
2021-02-13 01:41:58 +0200

    add github actions

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -1,13 +1,31 @@
+const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { merge } = require('webpack-merge');
 
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
   mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(process.cwd(), 'dist'),
+  },
   plugins: [
+    new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
       template: './public/index.html',
+    }),
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remote.js`,
+      },
+      shared: Object.keys(packageJson.dependencies),
     }),
   ],
 };
--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -1,13 +1,26 @@
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { merge } = require('webpack-merge');
 
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const prodConfig = {
   mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(process.cwd(), 'dist'),
+  },
   plugins: [
-    new HTMLWebpackPlugin({
-      template: './public/index.html',
+    new CleanWebpackPlugin(),
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remote.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: Object.keys(packageJson.dependencies),
     }),
   ],
 };
